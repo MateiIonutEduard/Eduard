5d1d7a553dcff9dd8a0440c861989bab87c95a2c
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-08-23 00:13:36 +0300

    Add the BasePoint field for the twisted Edwards curve to get a random base point or set a specified one #5
    Add the BasePoint field for the twisted Edwards curve to get a random base point or set a specified one.

--- Eduard/Cryptography/TwistedEdwardsCurve.cs
+++ Eduard/Cryptography/TwistedEdwardsCurve.cs
@@ -81,6 +81,72 @@ namespace Eduard.Cryptography
         }
 
         /// <summary>
+        /// Get a random base point or set a specified base point on the elliptic curve.
+        /// </summary>
+        public ECPoint BasePoint
+        {
+            get
+            {
+                bool done = false;
+                BigInteger x = 0;
+
+                BigInteger y = 0;
+                BigInteger temp = 0;
+
+                do
+                {
+                    x = BigInteger.Next(rand, 0, field - 1);
+                    temp = Evaluate(x);
+
+                    if (temp < 2)
+                        return new ECPoint(x, temp);
+
+                    if (BigInteger.Jacobi(temp, field) == 1)
+                    {
+                        done = true;
+                        y = Sqrt(temp);
+
+                        BigInteger eval = (y * y) % field;
+                        if (temp != eval) done = false;
+
+                        if (done)
+                        {
+                            ECPoint tempPoint = new ECPoint(x, y);
+                            basePoint = TwistedEdwardsMath.Multiply(this, cofactor, tempPoint, ECMode.EC_STANDARD_PROJECTIVE);
+                            done = (basePoint != ECPoint.POINT_INFINITY);
+                        }
+                    }
+                }
+                while (!done);
+
+                return basePoint;
+            }
+            set
+            {
+                ECPoint tempPoint = value;
+                var temp = Evaluate(tempPoint.GetAffineX());
+
+                if (BigInteger.Jacobi(temp, field) != 1 && temp > 0)
+                    throw new Exception("The generator point is not on the twisted Edwards curve.");
+                else
+                {
+                    BigInteger y = tempPoint.GetAffineY();
+                    BigInteger eval = (y * y) % field;
+
+                    if (eval != temp)
+                        throw new Exception("Invalid generator point for the twisted Edwards curve.");
+                    else
+                    {
+                        ECPoint point = TwistedEdwardsMath.Multiply(this, cofactor, tempPoint, ECMode.EC_STANDARD_PROJECTIVE);
+                        if (point != ECPoint.POINT_INFINITY) basePoint = tempPoint;
+                        else
+                            throw new Exception("Chosen generator point yields a small-order subgroup on the twisted Edwards curve.");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// Computes the modular square root of an integer over the prime field.
         /// </summary>
         /// <param name="val"></param>
