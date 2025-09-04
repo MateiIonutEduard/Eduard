2e8f51d7aae20021a0f5711961e526eb31f38867
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-09-04 21:16:16 +0300

    Add unified formula for point addition and doubling in extended projective coordinates #32
    Introduced the unified formula for point addition and doubling in extended projective coordinates to ensure consistency and efficiency in elliptic curve operations.

--- Eduard/Cryptography/TwistedEdwardsExtProjectiveMath.cs
+++ Eduard/Cryptography/TwistedEdwardsExtProjectiveMath.cs
@@ -12,6 +12,46 @@ namespace Eduard.Cryptography
     public static class TwistedEdwardsExtProjectiveMath
     {
         /// <summary>
+        /// Add two extended projective points on the twisted Edwards curve using the unified formula.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static ExtendedProjectivePoint UnifiedAdd(TwistedEdwardsCurve curve, ExtendedProjectivePoint left, ExtendedProjectivePoint right)
+        {
+            if (left == ExtendedProjectivePoint.POINT_INFINITY) return right;
+            if (right == ExtendedProjectivePoint.POINT_INFINITY) return left;
+
+            BigInteger p = curve.field;
+            BigInteger A1 = (left.x * right.x) % p;
+
+            BigInteger A2 = (left.y * right.y) % p;
+            BigInteger A3 = (curve.d * ((left.t * right.t) % p)) % p;
+
+            BigInteger A4 = (left.z * right.z) % p;
+            BigInteger A5 = ((((left.x + left.y) * (right.x + right.y)) % p) - A1 - A2) % p;
+
+            if (A5 < 0) A5 += p;
+            BigInteger A6 = (A4 - A3) % p;
+
+            if (A6 < 0) A6 += p;
+            BigInteger A7 = (A4 + A3) % p;
+
+            BigInteger A8 = (A2 - ((curve.a * A1) % p)) % p;
+            if (A8 < 0) A8 += p;
+
+            BigInteger X = (A5 * A6) % p;
+            BigInteger Y = (A7 * A8) % p;
+
+            BigInteger T = (A5 * A8) % p;
+            BigInteger Z = (A6 * A7) % p;
+
+            if (Z == 0) return ExtendedProjectivePoint.POINT_INFINITY;
+            return new ExtendedProjectivePoint(X, Y, T, Z);
+        }
+
+        /// <summary>
         /// Compute the additive inverse of a point in extended projective coordinates on the twisted Edwards curve.
         /// </summary>
         /// <param name="curve"></param>
