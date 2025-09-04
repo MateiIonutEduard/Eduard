e84c702071474523a0e95fc0c08bfef6baab5619
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-09-04 21:24:53 +0300

    Add dedicated formula for point addition in extended projective coordinates on twisted Edwards curve #32
    Add dedicated formula for point addition in extended projective coordinates on twisted Edwards curve

--- Eduard/Cryptography/TwistedEdwardsExtProjectiveMath.cs
+++ Eduard/Cryptography/TwistedEdwardsExtProjectiveMath.cs
@@ -52,6 +52,43 @@ namespace Eduard.Cryptography
         }
 
         /// <summary>
+        /// Add two extended projective points on the twisted Edwards curve using the dedicated formula.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static ExtendedProjectivePoint DedicatedAdd(TwistedEdwardsCurve curve, ExtendedProjectivePoint left, ExtendedProjectivePoint right)
+        {
+            if (left == ExtendedProjectivePoint.POINT_INFINITY) return right;
+            if (right == ExtendedProjectivePoint.POINT_INFINITY) return left;
+
+            BigInteger p = curve.field;
+            BigInteger A1 = (left.x * right.x) % p;
+
+            BigInteger A2 = (left.y * right.y) % p;
+            BigInteger A3 = (left.z * right.t) % p;
+
+            BigInteger A4 = (left.t * right.z) % p;
+            BigInteger A5 = (A4 + A3) % p;
+
+            BigInteger A6 = ((((left.x - right.y) * (right.x + right.y)) % p) + A2 - A1) % p;
+            if (A6 < 0) A6 += p;
+
+            BigInteger A7 = (A2 + curve.a * A1) % p;
+            BigInteger A8 = (p + A4 - A3) % p;
+
+            BigInteger X = (A5 * A6) % p;
+            BigInteger Y = (A7 * A8) % p;
+
+            BigInteger T = (A5 * A8) % p;
+            BigInteger Z = (A6 * A7) % p;
+
+            if (Z == 0) return ExtendedProjectivePoint.POINT_INFINITY;
+            return new ExtendedProjectivePoint(X, Y, T, Z);
+        }
+
+        /// <summary>
         /// Compute the additive inverse of a point in extended projective coordinates on the twisted Edwards curve.
         /// </summary>
         /// <param name="curve"></param>
