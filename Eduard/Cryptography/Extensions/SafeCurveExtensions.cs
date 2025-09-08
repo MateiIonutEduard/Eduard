0dc157cbc0516a8670eb45447d2ff31076350a87
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-09-08 18:54:10 +0300

    Check affine points for small order on Weierstrass curves #42
    Add a function to determine whether an affine point on a Weierstrass-form elliptic curve has a small order, improving security checks against weak points.

---
+++ Eduard/Cryptography/Extensions/SafeCurveExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduard.Cryptography.Extensions
+{
+    internal static class SafeCurveExtensions
+    {
+        internal static bool ValidatePoint(this EllipticCurve curve, ECPoint point)
+        {
+            ECPoint result = ECPoint.POINT_INFINITY;
+            int t = curve.cofactor.GetBits();
+            BigInteger k = curve.cofactor;
+
+            JacobianPoint auxPoint = JacobianPoint.POINT_INFINITY;
+            var basePoint = curve.ToModifiedJacobian(point);
+
+            for (int j = 0; j < t; j++)
+            {
+                if (k.TestBit(j))
+                    auxPoint = JacobianMath.Add(curve, auxPoint, curve.ToJacobian(basePoint));
+
+                basePoint = ModifiedJacobianMath.Doubling(curve, basePoint);
+            }
+
+            result = curve.ToAffine(auxPoint);
+            return (result != ECPoint.POINT_INFINITY);
+        }
+    }
+}
