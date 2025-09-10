939ed920bb0ba85cd75f05b2ca9d4805649f71f0
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-09-10 21:43:21 +0300

    Add method to compress affine points on the twisted Edwards curve #46
    Introduces a method for compressing affine points on the twisted Edwards curve, providing a compact representation useful in elliptic curve protocols and cryptographic applications.

--- Eduard/Cryptography/Extensions/PointEncodingExtensions.cs
+++ Eduard/Cryptography/Extensions/PointEncodingExtensions.cs
@@ -55,6 +55,49 @@ namespace Eduard.Cryptography.Extensions
         }
 
         /// <summary>
+        /// Compresses an affine point on the twisted Edwards curve into a byte array.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public static byte[] CompressPoint(this TwistedEdwardsCurve curve, ECPoint point, ECPointCompressionMode mode = ECPointCompressionMode.EC_POINT_COMPRESSED)
+        {
+            if (point == ECPoint.POINT_INFINITY)
+                throw new ArgumentException("Point at infinity cannot be compressed.");
+
+            if (mode == ECPointCompressionMode.EC_POINT_COMPRESSED)
+            {
+                /* compressed form */
+                byte[] bytes = point.GetAffineY().ToByteArray();
+                int n = curve.field.ToByteArray().Length;
+
+                byte[] result = new byte[n + 1];
+                Array.Copy(bytes, 0, result, 0, bytes.Length);
+
+                int sign = point.GetAffineX().TestBit(0) ? 1 : 0;
+                int lastIndex = result.Length - 1;
+
+                result[lastIndex] = (byte)(sign + 2);
+                return result;
+            }
+            else
+            {
+                /* uncompressed form */
+                int n = curve.field.ToByteArray().Length;
+                byte[] buffer = new byte[2 * n + 1];
+
+                byte[] xbuffer = point.GetAffineX().ToByteArray();
+                byte[] ybuffer = point.GetAffineY().ToByteArray();
+
+                Array.Copy(xbuffer, 0, buffer, 0, xbuffer.Length);
+                Array.Copy(ybuffer, 0, buffer, xbuffer.Length, ybuffer.Length);
+
+                buffer[2 * n] = 4;
+                return buffer;
+            }
+        }
+
+        /// <summary>
         /// Decompresses a byte array into an affine point on the Weierstrass curve.
         /// </summary>
         /// <param name="curve"></param>
