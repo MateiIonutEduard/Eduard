3742efe80a919806cb183b1d2c541dfd8d771d33
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2025-09-10 13:17:22 +0300

    Add procedure to decompress affine points on the Weierstrass curve #45
    Implement a function that takes a byte array and returns the corresponding affine point on the Weierstrass curve.

--- Eduard/Cryptography/Extensions/PointEncodingExtensions.cs
+++ Eduard/Cryptography/Extensions/PointEncodingExtensions.cs
@@ -17,7 +17,7 @@ namespace Eduard.Cryptography.Extensions
         public static byte[] CompressPoint(this EllipticCurve curve, ECPoint point)
         {
             if (point == ECPoint.POINT_INFINITY)
-                throw new ArgumentException("Point at infinity cannot be compressed");
+                throw new ArgumentException("Point at infinity cannot be compressed.");
 
             byte[] bytes = point.GetAffineX().ToByteArray();
             byte[] result = new byte[bytes.Length + 1];
@@ -28,6 +28,35 @@ namespace Eduard.Cryptography.Extensions
 
             result[lastIndex] = (byte)(sign + 2);
             return result;
+        }
+
+        /// <summary>
+        /// Decompresses a byte array into an affine point on the Weierstrass curve.
+        /// </summary>
+        /// <param name="curve"></param>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static ECPoint DecompressPoint(this EllipticCurve curve, byte[] bytes)
+        {
+            if(bytes == null) throw new ArgumentNullException("The byte array cannot be null.");
+            int n = curve.field.ToByteArray().Length;
+
+            if (bytes.Length == 0 || bytes.Length > n)
+                throw new ArgumentException("Invalid byte array length or corrupted data.");
+
+            byte[] data = new byte[bytes.Length - 1];
+            Array.Copy(bytes, data, bytes.Length - 1);
+
+            BigInteger Xp = new BigInteger(data);
+            BigInteger Yp = curve.Sqrt(curve.Evaluate(Xp), true);
+
+            int sign = bytes[bytes.Length - 1] - 2;
+            int y_sign = Yp.TestBit(0) ? 1 : 0;
+
+            BigInteger p = curve.field;
+            if (y_sign != sign) Yp = p - Yp;
+
+            return new ECPoint(Xp, Yp);
         }
     }
 }
