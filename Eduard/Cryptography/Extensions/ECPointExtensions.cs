d0445b0d6c8eb483d95125822d4948278cb926e6
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2026-02-26 12:01:16 +0200

    refactor: harmonize parameter naming in ECPointExtensions
    - Rename method parameters to 'point' across all conversion methods
        - Remove redundant parameter qualifiers (jacobianPoint → point, affinePoint → point)
        - Maintain consistency with elliptic curve cryptography naming conventions
        - No functional changes, purely cosmetic cleanup

--- Eduard/Cryptography/Extensions/ECPointExtensions.cs
+++ Eduard/Cryptography/Extensions/ECPointExtensions.cs
@@ -14,20 +14,20 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a given point in Jacobian projective form to an affine point on the elliptic curve.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="jacobianPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ECPoint ToAffine(this EllipticCurve curve, JacobianPoint jacobianPoint)
+        public static ECPoint ToAffine(this EllipticCurve curve, JacobianPoint point)
         {
-            if (jacobianPoint == JacobianPoint.POINT_INFINITY || jacobianPoint.z == 0) 
+            if (point == JacobianPoint.POINT_INFINITY || point.z == 0) 
                 return ECPoint.POINT_INFINITY;
 
             BigInteger p = curve.field;
-            BigInteger Z2 = (jacobianPoint.z * jacobianPoint.z) % p;
+            BigInteger Z2 = (point.z * point.z) % p;
 
-            BigInteger Z3 = (Z2 * jacobianPoint.z) % p;
-            BigInteger X = (jacobianPoint.x * Z2.Inverse(p)) % p;
+            BigInteger Z3 = (Z2 * point.z) % p;
+            BigInteger X = (point.x * Z2.Inverse(p)) % p;
 
-            BigInteger Y = (jacobianPoint.y * Z3.Inverse(p)) % p;
+            BigInteger Y = (point.y * Z3.Inverse(p)) % p;
             return new ECPoint(X, Y);
         }
 
@@ -162,15 +162,15 @@ namespace Eduard.Cryptography.Extensions
         /// Convert an affine point on the elliptic curve to Jacobian projective form.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="affinePoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static JacobianPoint ToJacobian(this EllipticCurve curve, ECPoint affinePoint)
+        public static JacobianPoint ToJacobian(this EllipticCurve curve, ECPoint point)
         {
-            if (affinePoint == ECPoint.POINT_INFINITY) 
+            if (point == ECPoint.POINT_INFINITY) 
                 return JacobianPoint.POINT_INFINITY;
 
-            JacobianPoint jacobianPoint = new JacobianPoint(affinePoint.GetAffineX(),
-                affinePoint.GetAffineY(), 1);
+            JacobianPoint jacobianPoint = new JacobianPoint(point.GetAffineX(),
+                point.GetAffineY(), 1);
 
             return jacobianPoint;
         }
@@ -179,15 +179,15 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a given point in modified Jacobian projective coordinates into a Jacobian projective point on the elliptic curve.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="modifiedPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static JacobianPoint ToJacobian(this EllipticCurve curve, ModifiedJacobianPoint modifiedPoint)
+        public static JacobianPoint ToJacobian(this EllipticCurve curve, ModifiedJacobianPoint point)
         {
-            if (modifiedPoint == ModifiedJacobianPoint.POINT_INFINITY) 
+            if (point == ModifiedJacobianPoint.POINT_INFINITY) 
                 return JacobianPoint.POINT_INFINITY;
 
-            JacobianPoint jacobianPoint = new JacobianPoint(modifiedPoint.x,
-                modifiedPoint.y, modifiedPoint.z);
+            JacobianPoint jacobianPoint = new JacobianPoint(point.x,
+                point.y, point.z);
 
             return jacobianPoint;
         }
@@ -196,15 +196,15 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a given point in extended Jacobian-Chudnovsky projective coordinates into a Jacobian projective point on the elliptic curve.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="jacobianChudnovskyPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static JacobianPoint ToJacobian(this EllipticCurve curve, JacobianChudnovskyPoint jacobianChudnovskyPoint)
+        public static JacobianPoint ToJacobian(this EllipticCurve curve, JacobianChudnovskyPoint point)
         {
-            if (jacobianChudnovskyPoint == JacobianChudnovskyPoint.POINT_INFINITY)
+            if (point == JacobianChudnovskyPoint.POINT_INFINITY)
                 return JacobianPoint.POINT_INFINITY;
 
-            JacobianPoint jacobianPoint = new JacobianPoint(jacobianChudnovskyPoint.x,
-                jacobianChudnovskyPoint.y, jacobianChudnovskyPoint.z);
+            JacobianPoint jacobianPoint = new JacobianPoint(point.x,
+                point.y, point.z);
 
             return jacobianPoint;
         }
@@ -213,15 +213,15 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a given point in extended Jacobian-Chudnovsky projective coordinates into an affine point on the elliptic curve.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="jacobianChudnovskyPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ECPoint ToAffine(this EllipticCurve curve, JacobianChudnovskyPoint jacobianChudnovskyPoint)
+        public static ECPoint ToAffine(this EllipticCurve curve, JacobianChudnovskyPoint point)
         {
-            if (jacobianChudnovskyPoint == JacobianChudnovskyPoint.POINT_INFINITY || jacobianChudnovskyPoint.z == 0) return ECPoint.POINT_INFINITY;
+            if (point == JacobianChudnovskyPoint.POINT_INFINITY || point.z == 0) return ECPoint.POINT_INFINITY;
             BigInteger p = curve.field;
 
-            BigInteger X = (jacobianChudnovskyPoint.x * jacobianChudnovskyPoint.z2.Inverse(p)) % p;
-            BigInteger Y = (jacobianChudnovskyPoint.y * jacobianChudnovskyPoint.z3.Inverse(p)) % p;
+            BigInteger X = (point.x * point.z2.Inverse(p)) % p;
+            BigInteger Y = (point.y * point.z3.Inverse(p)) % p;
             return new ECPoint(X, Y);
         }
 
@@ -229,12 +229,12 @@ namespace Eduard.Cryptography.Extensions
         /// Convert an affine point on the elliptic curve to the extended Jacobian-Chudnovsky projective form.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="affinePoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static JacobianChudnovskyPoint ToJacobianChudnovsky(this EllipticCurve curve, ECPoint affinePoint)
+        public static JacobianChudnovskyPoint ToJacobianChudnovsky(this EllipticCurve curve, ECPoint point)
         {
-            if (affinePoint == ECPoint.POINT_INFINITY) return JacobianChudnovskyPoint.POINT_INFINITY;
-            JacobianChudnovskyPoint jacobianChudnovskyPoint = new JacobianChudnovskyPoint(affinePoint.GetAffineX(), affinePoint.GetAffineY(), 1, 1, 1);
+            if (point == ECPoint.POINT_INFINITY) return JacobianChudnovskyPoint.POINT_INFINITY;
+            JacobianChudnovskyPoint jacobianChudnovskyPoint = new JacobianChudnovskyPoint(point.GetAffineX(), point.GetAffineY(), 1, 1, 1);
             return jacobianChudnovskyPoint;
         }
 
@@ -242,18 +242,18 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a given point in modified Jacobian projective coordinates into an affine point on the elliptic curve.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="modifiedJacobianPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ECPoint ToAffine(this EllipticCurve curve, ModifiedJacobianPoint modifiedJacobianPoint)
+        public static ECPoint ToAffine(this EllipticCurve curve, ModifiedJacobianPoint point)
         {
-            if (modifiedJacobianPoint == ModifiedJacobianPoint.POINT_INFINITY || modifiedJacobianPoint.z == 0) return ECPoint.POINT_INFINITY;
+            if (point == ModifiedJacobianPoint.POINT_INFINITY || point.z == 0) return ECPoint.POINT_INFINITY;
             BigInteger p = curve.field;
 
-            BigInteger Z2 = (modifiedJacobianPoint.z * modifiedJacobianPoint.z) % p;
-            BigInteger Z3 = (Z2 * modifiedJacobianPoint.z) % p;
-            BigInteger X = (modifiedJacobianPoint.x * Z2.Inverse(p)) % p;
+            BigInteger Z2 = (point.z * point.z) % p;
+            BigInteger Z3 = (Z2 * point.z) % p;
+            BigInteger X = (point.x * Z2.Inverse(p)) % p;
 
-            BigInteger Y = (modifiedJacobianPoint.y * Z3.Inverse(p)) % p;
+            BigInteger Y = (point.y * Z3.Inverse(p)) % p;
             return new ECPoint(X, Y);
         }
 
@@ -261,12 +261,12 @@ namespace Eduard.Cryptography.Extensions
         /// Convert an affine point on the elliptic curve into a given point in modified Jacobian projective coordinates.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="affinePoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, ECPoint affinePoint)
+        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, ECPoint point)
         {
-            if (affinePoint == ECPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
-            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(affinePoint.GetAffineX(), affinePoint.GetAffineY(), 1, curve.a);
+            if (point == ECPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
+            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(point.GetAffineX(), point.GetAffineY(), 1, curve.a);
             return modifiedJacobianPoint;
         }
 
@@ -274,15 +274,15 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a point from Jacobian-Chudnovsky form on the elliptic curve into a given point in modified Jacobian projective coordinates.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="jacobianChudnovskyPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, JacobianChudnovskyPoint jacobianChudnovskyPoint)
+        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, JacobianChudnovskyPoint point)
         {
-            if (jacobianChudnovskyPoint == JacobianChudnovskyPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
-            BigInteger Z4 = (jacobianChudnovskyPoint.z2 * jacobianChudnovskyPoint.z2) % curve.field;
+            if (point == JacobianChudnovskyPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
+            BigInteger Z4 = (point.z2 * point.z2) % curve.field;
 
             BigInteger aZ4 = (curve.a * Z4) % curve.field;
-            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(jacobianChudnovskyPoint.x, jacobianChudnovskyPoint.y, jacobianChudnovskyPoint.z, aZ4);
+            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(point.x, point.y, point.z, aZ4);
             return modifiedJacobianPoint;
         }
 
@@ -290,16 +290,16 @@ namespace Eduard.Cryptography.Extensions
         /// Convert a point from Jacobian form on the elliptic curve into a given point in modified Jacobian projective coordinates.
         /// </summary>
         /// <param name="curve"></param>
-        /// <param name="jacobianPoint"></param>
+        /// <param name="point"></param>
         /// <returns></returns>
-        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, JacobianPoint jacobianPoint)
+        public static ModifiedJacobianPoint ToModifiedJacobian(this EllipticCurve curve, JacobianPoint point)
         {
-            if (jacobianPoint == JacobianPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
-            BigInteger Z2 = (jacobianPoint.z * jacobianPoint.z) % curve.field;
+            if (point == JacobianPoint.POINT_INFINITY) return ModifiedJacobianPoint.POINT_INFINITY;
+            BigInteger Z2 = (point.z * point.z) % curve.field;
             BigInteger Z4 = (Z2 * Z2) % curve.field;
 
             BigInteger aZ4 = (curve.a * Z4) % curve.field;
-            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(jacobianPoint.x, jacobianPoint.y, jacobianPoint.z, aZ4);
+            ModifiedJacobianPoint modifiedJacobianPoint = new ModifiedJacobianPoint(point.x, point.y, point.z, aZ4);
             return modifiedJacobianPoint;
         }
     }
