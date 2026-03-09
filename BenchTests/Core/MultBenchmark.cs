8666bb72ac3e42b98d367310486cdf88264c65af
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2026-03-09 19:03:21 +0200

    feat: add big integer multiplication benchmarks
    Add BenchmarkTests project with comprehensive benchmarks for big integer multiplication and squaring operations, comparing different algorithmic approaches (standard, Karatsuba, NTT) across various word sizes from 8 to 2048 words.

---
+++ BenchTests/Core/MultBenchmark.cs
@@ -0,0 +1,79 @@
+﻿#if RELEASE && USE_BENCHMARKING
+using Eduard;
+using System;
+using BenchmarkDotNet.Attributes;
+using CoreCC = System.Security.Cryptography;
+#pragma warning disable
+
+namespace BenchmarkTests.BenchCore.BigInt
+{
+    public class MultBenchmark
+    {
+        [Params(8, 16, 32, 64, 128, 256, 512, 1024, 1152, 1280, 1408, 1536, 1664, 1792, 1920, 2048)]
+        public int words;
+
+        private CoreCC.RandomNumberGenerator rand;
+        private BigInteger left, field;
+        private BigInteger right;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            rand = CoreCC.RandomNumberGenerator.Create();
+            int bits = words << 5;
+
+            left = new BigInteger(bits, rand);
+            right = new BigInteger(bits, rand);
+        }
+
+        [Benchmark]
+        public void StandardBigIntMultiplication()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words << 8);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_MULTIPLY, words << 4);
+            BigInteger res = left * right;
+        }
+
+        [Benchmark]
+        public void StandardBigIntSquaring()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words << 8);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_SQUARING, words << 4);
+            BigInteger res = left * left;
+        }
+
+        [Benchmark]
+        public void KaratsubaBigIntMultiplication()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words << 2);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_MULTIPLY, words);
+            BigInteger res = left * right;
+        }
+
+        [Benchmark]
+        public void KaratsubaBigIntSquaring()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words << 2);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_SQUARING, words);
+            BigInteger res = left * left;
+        }
+
+        [Benchmark]
+        public void NTTBigIntMultiplication()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_MULTIPLY, words >> 2);
+            BigInteger res = left * right;
+        }
+
+        [Benchmark]
+        public void NTTBigIntSquaring()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_FFT, words);
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_KARATSUBA_SQUARING, words >> 2);
+            BigInteger res = left * left;
+        }
+    }
+}
+
+#endif
---
+++ BenchTests/Program.cs
@@ -0,0 +1,19 @@
+﻿#if RELEASE && USE_BENCHMARKING
+using BenchmarkDotNet.Running;
+using BenchmarkTests.BenchCore.BigInt;
+#else
+using System;
+#endif
+
+namespace BenchTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE && USE_BENCHMARKING
+            BenchmarkRunner.Run<MultBenchmark>();
+#endif
+        }
+    }
+}
