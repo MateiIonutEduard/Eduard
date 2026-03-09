cf0c0304783d9f7aaca7ee362c468cb6ef2549cd
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2026-03-09 19:13:38 +0200

    perf: add modular exponentiation benchmarks
    Introduce benchmark suite comparing binary vs sliding window
    exponentiation methods for modular exponentiation across prime field sizes from 128 to 1024 bits, with dynamic algorithm thresholds for performance optimization.

---
+++ BenchTests/Core/BigInt/ExpoBenchmark.cs
@@ -0,0 +1,42 @@
+﻿#if RELEASE && USE_BENCHMARKING
+using BenchmarkDotNet.Attributes;
+using System;
+using Eduard;
+using CoreCC = System.Security.Cryptography;
+#pragma warning disable
+
+namespace BenchmarkTests.Core.BigInt
+{
+    public class ExpoBenchmark
+    {
+        [Params(128, 160, 192, 256, 320, 384, 512, 768, 1024)]
+        public int bits;
+
+        private CoreCC.RandomNumberGenerator rand;
+        private BigInteger val, field;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            rand = CoreCC.RandomNumberGenerator.Create();
+            field = BigInteger.GenProbablePrime(rand, bits, 50);
+            val = BigInteger.Next(rand, 1, field - 1);
+        }
+
+        [Benchmark]
+        public void BinaryExponentiation()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_WORDS_THRESHOLD, bits >> 4);
+            BigInteger res = BigInteger.Pow(val, (field - 1) >> 1, field);
+        }
+
+        [Benchmark]
+        public void SlidingWindowExponentiation()
+        {
+            PerfTuner.SetThreshold(PerfEntry.BIGINT_WORDS_THRESHOLD, bits >> 5);
+            BigInteger res = BigInteger.Pow(val, (field - 1) >> 1, field);
+        }
+    }
+}
+
+#endif
