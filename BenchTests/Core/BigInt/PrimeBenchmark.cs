2967af4370a708f18d4956953032c94535eea578
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2026-03-09 19:16:59 +0200

    feat: add probable prime generation benchmarks
    Add benchmark for cryptographic prime generation across common
    ECC key sizes (256-1024 bits) to measure performance of probabilistic primality testing with 50 rounds of Miller-Rabin.

---
+++ BenchTests/Core/BigInt/PrimeBenchmark.cs
@@ -0,0 +1,30 @@
+﻿#if RELEASE && USE_BENCHMARKING
+using BenchmarkDotNet.Attributes;
+using System;
+using Eduard;
+using CoreCC = System.Security.Cryptography;
+#pragma warning disable
+
+namespace BenchmarkTests.Core.BigInt
+{
+    public class PrimeBenchmark
+    {
+        [Params(256, 320, 384, 512, 768, 1024)]
+        public int bits;
+        private CoreCC.RandomNumberGenerator rand;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            rand = CoreCC.RandomNumberGenerator.Create();
+        }
+
+        [Benchmark]
+        public void GenProbablePrime()
+        {
+            BigInteger field = BigInteger.GenProbablePrime(rand, bits, 50);
+        }
+    }
+}
+
+#endif
