8ff9c8a5ef78b79eaf83c683a65714d67bfa7b7b
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2026-03-09 19:29:47 +0200

    perf: add polynomial modular exponentiation benchmarks
    Implement benchmark for polynomial exponentiation over prime fields,
    measuring X^p mod f(x) performance across varying field sizes (160-512 bits) and modulus degrees (16-512). Essential for cryptographic operations like Frobenius endomorphism and field
    extension arithmetic.

---
+++ BenchTests/Core/Poly/PowModBenchmark.cs
@@ -0,0 +1,43 @@
+﻿#if RELEASE && USE_BENCHMARKING
+using Eduard;
+using Eduard.Security;
+using BenchmarkDotNet.Attributes;
+using CoreCC = System.Security.Cryptography;
+#pragma warning disable
+
+namespace BenchmarkTests.Core.Poly
+{
+    public class PowModBenchmark
+    {
+        [Params(160, 192, 256, 384, 512)]
+        public int bitSize;
+
+        [Params(16, 32, 64, 96, 128, 256, 512)]
+        public int degree;
+
+        private CoreCC.RandomNumberGenerator rand;
+        private Polynomial mod;
+        private BigInteger field;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            rand = CoreCC.RandomNumberGenerator.Create();
+            field = BigInteger.GenProbablePrime(rand, bitSize, 50);
+
+            Polynomial.SetField(field);
+            mod = new Polynomial(degree);
+
+            for (int i = 0; i <= degree; i++)
+                mod.coeffs[i] = BigInteger.Next(rand, 1, field - 1);
+        }
+
+        [Benchmark]
+        public void PolyPowMod()
+        {
+            Polynomial X = new Polynomial(1, 0);
+            Polynomial XP = Polynomial.Pow(X, field, mod);
+        }
+    }
+}
+#endif
